df5351b095bed7e31a297322da2ae756bfcaf9c9
atoonft <atoonft@gmail.com>
2021-11-02 23:00:39 -0500

    Actualización diseño de templates

--- src/main/resources/static/js/Productos.js
+++ src/main/resources/static/js/Productos.js
@@ -9,9 +9,19 @@ function direccionar(n){
 	document.getElementsByClassName("rs").href=lista(n);
 }
 
-
+   
 $(document).ready(function(){
-	
+	 $(window).scroll(function(){     
+        //scroll-up
+        if(this.scrollY > 450){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+        
+    });
+    
+    
 	var lista=[
 	"www.facebook.com",
 	"www.youtube.com"
@@ -19,6 +29,7 @@ $(document).ready(function(){
 function direccionar(n){
 	document.getElementsByClassName("rs").href=lista(n);
 }	
+	
 	$("#baner").cycle({
 		'fx':'scrollLeft',
 		'timeout':1000,
@@ -36,5 +47,20 @@ function direccionar(n){
 	$(this).find("p").fadeOut(500);
 	}	
 	);
+
+   
+	
+	 //slider del dedo
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+    
+	//animar letra
+    var typed = new Typed(".letra", {
+        strings: ["mejor precio"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 
 });
