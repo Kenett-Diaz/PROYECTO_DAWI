e5eb285f61513d97ce647ffedbae17222660ae52
Alejandra <marialejandra.pazos@apisdev.com>
2021-11-17 00:29:52 -0500

    agregando clases

---
+++ src/main/java/com/electro/models/Cliente.java
@@ -0,0 +1,28 @@
+package com.electro.models;
+
+public class Cliente {
+
+	private int codCliente;
+	private String nombreCliente;
+	private String apeCliente;
+	public int getCodCliente() {
+		return codCliente;
+	}
+	public void setCodCliente(int codCliente) {
+		this.codCliente = codCliente;
+	}
+	public String getNombreCliente() {
+		return nombreCliente;
+	}
+	public void setNombreCliente(String nombreCliente) {
+		this.nombreCliente = nombreCliente;
+	}
+	public String getApeCliente() {
+		return apeCliente;
+	}
+	public void setApeCliente(String apeCliente) {
+		this.apeCliente = apeCliente;
+	}
+	
+	
+}
